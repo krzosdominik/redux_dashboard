f92172bc634f7824b080cbcbbccf854a2dbf00d4
krzosdominik <krzosdominik@gmail.com>
2020-04-25 14:18:50 +0200

    add bootstrap and fontawesome

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './store';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 import App from './App';
 
