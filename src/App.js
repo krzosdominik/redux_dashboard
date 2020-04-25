283a889f75bc360a8f37a2c0c1631d1f6a634f52
krzosdominik <krzosdominik@gmail.com>
2020-04-25 14:20:01 +0200

    connect component with App.js and add routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { Container } from 'react-bootstrap';
 
+import Main from './app/components/MainPage';
+import { AddUser, EditUser} from './app/components/UserManagement';
+import { getAllUsers } from './app/users/duck/operations';
+
 const App = () => {
-  
-  return (
-      <Container>
-          <header>
-              <h1>Dashboard</h1>
-          </header>
-      </Container>
-  );
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getAllUsers())
+    });
+
+    return (
+        <Container>
+            <header>
+                <h1>Dashboard</h1>
+            </header>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Main} />
+                    <Route path="/user/add" component={AddUser} />
+                    <Route path="/user/:id" component={EditUser} />
+                </Switch>
+            </Router>
+        </Container>
+    );
 }
 
 export default App;
