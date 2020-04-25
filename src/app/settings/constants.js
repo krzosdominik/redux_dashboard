e41842b601362fc4a7d1a98a999e2e53fdb16cb6
krzosdominik <krzosdominik@gmail.com>
2020-04-25 13:23:57 +0200

    add constants

---
+++ src/app/settings/constants.js
@@ -0,0 +1,26 @@
+export const tableHeaders = ["Id", "Name", "Username", "Email", "City", "Edit", "Delete"];
+
+export const API_URL = 'https://jsonplaceholder.typicode.com/users';
+
+const headers = {
+    "Content-type": "application/json; charset=UTF-8"
+};
+
+export const GET = {
+    method: 'GET',
+    headers
+};
+
+export const POST = {
+    method: 'POST',
+    headers
+};
+
+export const PUT = {
+    method: 'PUT',
+    headers
+};
+
+export const DELETE = {
+    method: 'DELETE'
+}
